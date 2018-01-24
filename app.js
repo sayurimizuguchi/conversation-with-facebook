ac8eb9d065a933cb9d8704d31f69f8f7835c3481
Sayuri Mizuguchi <adoravel_sansei@hotmail.com>
2018-01-24 10:56:55 -0200

    update

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
+'use strict';
+
+require('dotenv').config({ silent: true });
+
 var express = require('express'); //app server
 var request = require('request'); //request module to make http requests
 var bodyParser = require('body-parser'); // parser for post requests
-var watson = require('watson-developer-cloud/conversation/v1'); // using only conversation service
+var Conversation = require('watson-developer-cloud/conversation/v1'); // watson sdk, using only conversation service
 
-// server config
-const token = process.env.TOKEN || "Or paste your FB token here";
-const host = process.env.VCAP_APP_HOST || 'localhost';
-const port = process.env.VCAP_APP_PORT || 3000;
-
+// server  express config
 var app = express();
-const contextID = "";
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const host = process.env.VCAP_APP_HOST || 'localhost'; //vcap for bluemix/cloud
+const port = process.env.VCAP_APP_PORT || 3000; //vcap port for bluemix/cloud
 
-var conversation = watson.conversation({
+//facebook token generated
+const token = process.env.TOKEN || "Or paste your FB token here";
+
+//Workspace from your Conversation Service
+const workspace = process.env.WORKSPACE_ID || 'Or paste your workspace_id here';
+
+var conversation = new Conversation({
     url: 'https://gateway.watsonplatform.net/conversation/api',
     username: process.env.CONVERSATION_USERNAME || 'Or paste here your username',
     password: process.env.CONVERSATION_PASSWORD || 'Or paste here your password',
@@ -22,81 +28,77 @@ var conversation = watson.conversation({
     version_date: '2017-05-26'
 });
 
-const workspace = process.env.WORKSPACE_ID || 'Or paste your workspace_id here';
-
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'Paste your FB token here') {
+app.get('/webhook/', function(req, res) {
+    if (req.query['hub.verify_token'] === 'EAACwpSURYj0BAL5FKEZA2pHbt4N9gfv8ikW9gvDs4B7tZBXA4iVTZBojD2SuIAwR1ivAMebaIUTcHQuHZA51m2iO2Lg54s6ZCK9FgjHaULzSgCuxyk2LQDCb0L98ZBvh5QuZALqi4hYxfIZCJuzY2FCbgqkxvc7KOuyVsueBozdbxAZDZD') {
         res.send(req.query['hub.challenge']);
     }
     res.send('Error when we try to validating your token.');
 });
 
-app.post('/webhook/', function (req, res) {
-	var text = null;
-	
+const contextID = "";
+app.post('/webhook/', function(req, res) {
+    var text = null;
+
     messaging_events = req.body.entry[0].messaging;
-	for (i = 0; i < messaging_events.length; i++) {	
+    for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i];
         res = event.res.id;
 
         if (event.message && event.message.text) {
-			text = event.message.text;
-    		} else if (event.postback && !text) {
-    			text = event.postback.payload;
-        		} else {
-        			break;
+            text = event.message.text;
+        } else if (event.postback && !text) {
+            text = event.postback.payload;
+        } else {
+            break;
         }
-		
-		var params = {
-			input: text,
-			context: contextID
-		};
 
-		var payload = {
-			workspace_id: workspace
-		};
+        var params = {
+            input: text,
+            context: contextID
+        };
 
-		if (params) {
-			if (params.input) {
-				params.input = params.input.replace("\n", "");
-				payload.input = { "text": params.input };
-			}
-			if (params.context) {
-				payload.context = params.context;
-			}
-		}
-		callMessage(payload, res);
+        var payload = {
+            workspace_id: workspace
+        };
+
+        if (params) {
+            if (params.input) {
+                params.input = params.input.replace("\n", "");
+                payload.input = { "text": params.input };
+            }
+            if (params.context) {
+                payload.context = params.context;
+            }
+        }
+        callMessage(payload, res);
     }
     res.sendStatus(200);
 });
 
 function callMessage(payload, res) {
     var conversation_id = "";
-	conversation.message(payload, function (err, convResults) {
-		console.log(convResults);
-		contextid = convResults.context;
-		
-        if (err) {
-            return responseToRequest.send("Error");
+    conversation.message(payload, function(err, responseMessage) {
+        console.log(responseMessage);
+        contextID = responseMessage.context;
+
+        if (err) return responseToRequest.send("Error");
+
+        if (responseMessage.context != null) conversation_id = responseMessage.context.conversation_id;
+
+        if (responseMessage != null && responseMessage.output != null) {
+            var i = 0;
+            while (i < responseMessage.output.text.length) {
+                sendMessage(res, responseMessage.output.text[i++]);
+            }
         }
-		
-		if (convResults.context != null)
-    	   conversation_id = convResults.context.conversation_id;
-        if (convResults != null && convResults.output != null) {
-			var i = 0;
-			while(i < convResults.output.text.length) {
-				sendMessage(res, convResults.output.text[i++]);
-			}
-		}
-            
     });
 }
 
-function sendMessage(res, text_) {
-	text_ = text_.substring(0, 319);
-	messageData = {	text: text_ };
-	
-	var dataPost = {
+function sendMessage(res, outputText) {
+    outputText = outputText.substring(0, 319);
+    messageData = { text: outputText };
+
+    var dataPost = {
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: { access_token: token },
         method: 'POST',
@@ -106,7 +108,7 @@ function sendMessage(res, text_) {
         }
     };
 
-    request(dataPost, function (error, response, body) {
+    request(dataPost, function(error, response, body) {
         if (error) {
             console.log('Error when we try to sending message: ', error);
         } else if (response.body.error) {
@@ -116,5 +118,5 @@ function sendMessage(res, text_) {
 }
 
 app.listen(port, host, function() {
-  console.log('Server running on port: %d', port);
+    console.log('Server running on port: %d', port);
 });
