4757a1940fe0a419964b7db9d5af14b31727e152
Sayuri Mizuguchi <adoravel_sansei@hotmail.com>
2019-06-21 17:44:20 -0400

    Separating constants

---
+++ utils/constants.js
@@ -0,0 +1,10 @@
+export const env = {
+    username: process.env.CONVERSATION_USERNAME,
+    password: process.env.CONVERSATION_PASSWORD,
+    workspace: process.env.WORKSPACE_ID,
+    tokenFB: process.env.TOKEN,
+    url: process.env.WATSON_URL,
+    host: process.env.VCAP_APP_HOST,
+    port: process.env.VCAP_APP_PORT,
+    version: '2019-02-28'
+}
